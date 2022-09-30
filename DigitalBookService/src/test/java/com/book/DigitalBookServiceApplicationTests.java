c54d98ebcf8edcbad68ce787dac38e4668c20261
CK <chaitaligk36@gmail.com>
2022-09-30 15:24:45 +0530

    commit1

--- DigitalBookService/src/test/java/com/book/DigitalBookServiceApplicationTests.java
+++ DigitalBookService/src/test/java/com/book/DigitalBookServiceApplicationTests.java
@@ -5,6 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DigitalBookServiceApplicationTests {
+	
+	private void syso() {
+		// TODO Auto-generated method stub
+
+	}
 
 	@Test
 	void contextLoads() {
